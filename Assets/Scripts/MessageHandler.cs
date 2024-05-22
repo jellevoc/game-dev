55d4d3393fc0fd5d15be736597e0efec7e0f657e
Jelle Mac <93642321+jellevoc@users.noreply.github.com>
2024-05-22 16:48:32 +0200

    Changed menu background and you can now add custom message

--- Assets/Scripts/MessageHandler.cs
+++ Assets/Scripts/MessageHandler.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MessageHandler : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private Animator anim;
+    [SerializeField] private TextMeshProUGUI messageText;
 
     public static MessageHandler main;
+
+    private string baseText;
 
     private void Awake()
     {
         anim.gameObject.SetActive(false);
         main = this;
+    }
+
+    private void Start()
+    {
+        baseText = messageText.text;
     }
 
     public void ShowMessage()
@@ -27,5 +36,14 @@ public class MessageHandler : MonoBehaviour
         anim.SetBool("ShowMessage", true);
         yield return new WaitForSeconds(1);
         anim.SetBool("ShowMessage", false);
+    }
+
+    public void ShowCustomMessage(string _message)
+    {
+        messageText.text = _message;
+        anim.gameObject.SetActive(true);
+        if (anim.GetBool("ShowMessage") == true) return;
+        StartCoroutine(ShowAndHideMessage());
+        messageText.text = baseText;
     }
 }
